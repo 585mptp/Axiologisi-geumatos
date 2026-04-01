48edc78955bc27d533c72d620312404f62ee1aa8
UnLakhs <apostolos052@gmail.com>
2026-04-01 21:13:07 +0200

    test

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ initializeStorage();
 //test data for mongo
 async function sendTestData() {
     try {
-        const res = await fetch("/api/test/test.js", {
+        const res = await fetch("/api/test/test", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
