6b9582fdfe173e38ba95f40679c6ea9b50dd12b0
UnLakhs <apostolos052@gmail.com>
2026-04-01 22:57:23 +0200

    more testing

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ function getMealPeriod() {
     const hour = new Date().getHours();
     console.log("Current hour:", hour);
     if (hour < 11) return "breakfast";
-    if (hour < 17) return "dinner";
-    return "lunch";
+    if (hour < 17) return "lunch";
+    return "dinner";
 }
 
 function showThanksMessage() {
