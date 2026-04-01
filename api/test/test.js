9cf3287ea89ae72ab23c9e3407573e100cb390c0
UnLakhs <apostolos052@gmail.com>
2026-04-01 21:17:54 +0200

    test

--- api/test/test.js
+++ api/test/test.js
@@ -1,23 +1,28 @@
 import clientPromise from "../../lib/MongoDB";
 
 export default async function handler(req, res) {
+    console.log("API HIT");
+
     if (req.method !== "POST") {
         return res.status(405).json({ message: "Method not allowed" });
     }
 
     try {
+        console.log("Connecting to DB...");
+
         const client = await clientPromise;
         const db = client.db("meal-assessment");
-        const collection = db.collection("ratings");
 
-        const result = await collection.insertOne({
+        console.log("Connected!");
+
+        const result = await db.collection("ratings").insertOne({
             ...req.body,
             createdAt: new Date()
         });
 
         return res.status(200).json({ message: "Data saved", id: result.insertedId });
     } catch (error) {
-        console.error("Error inserting data:", error);
+        console.error("FULL ERROR:", error);
         return res.status(500).json({ message: "Internal server error" });
     }
-} 
+}
